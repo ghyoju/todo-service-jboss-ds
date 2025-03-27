a6eec07bc78bbe7cdca8e1c5e4507dabc0d3c667
decodingcloud <decodingcloud@gmail.com>
2025-03-27 12:10:56 -0500

    initial commit

--- src/main/java/com/example/todoservice/TodoService.java
+++ src/main/java/com/example/todoservice/TodoService.java
@@ -3,16 +3,14 @@ package com.example.todoservice;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebService;
 import jakarta.jws.soap.SOAPBinding;
+import com.example.entity.*;
 
 @WebService
 @SOAPBinding(style = SOAPBinding.Style.DOCUMENT)
 public interface TodoService {
     @WebMethod
     String addTodo(String task);
-    
+
     @WebMethod
-    String getTodos();
-    
-    @WebMethod
-    String getTodo(long id);
+    Todo getTodo(Long id);
 }
